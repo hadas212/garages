e47155dde9067e23c8c4fa5d05fc36a503356024
דסי <s0548414125@gmail.com>
2025-12-02 19:56:02 +0200

    add model to front

---
+++ my-garages-app/src/app/models/garage.model.ts
@@ -0,0 +1,16 @@
+export interface Garage {
+  id: number;
+  misparMosah: number;
+  name: string;
+  codSugMosah: number;
+  sugMosah: string;
+  address: string;
+  city: string;
+  telephone: string;
+  mikud: number;
+  codMiktzoa: number;
+  miktzoa: string;
+  menahelMiktzoa: string;
+  licenseNumber: number;
+  testime: string;
+}
